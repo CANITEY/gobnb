5352b963f8d8d081d79ba75aefc6511d4e9d00d7
CANITEY <mohamedmed01345@gmail.com>
2024-06-16 15:31:24 +0300

    added some fields to user and apartment models
    id for the apartment id and user id

--- models/apartment.go
+++ models/apartment.go
@@ -1,1 +1,10 @@
-package model
+package models
+
+type Apartment struct {
+	Id int `json:"id"`
+    Title string `json:"title"`
+	Description string `json:"description"`
+	Price float32 `json:"price"`
+	Rental bool `json:"Rental"`
+	Available bool `json:"Open"`
+}
--- models/user.go
+++ models/user.go
@@ -1,1 +1,8 @@
 package models
+
+type User struct {
+	Id int `json:"id"`
+    Name string `json:"name"`
+	Email string `json:"email"`
+	Phone string `json:"phone"`
+}
