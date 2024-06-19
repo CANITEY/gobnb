2d0932702c14351931d8cef51166a9aa751eff67
CANITEY <mohamedmed01345@gmail.com>
2024-06-19 18:34:12 +0300

    create a function to make a user model from urlvalues

--- models/user.go
+++ models/user.go
@@ -1,9 +1,19 @@
 package models
 
+import "net/url"
+
 type User struct {
 	Id int `json:"id"`
     Name string `json:"name"`
 	Email string `json:"email"`
 	Phone string `json:"phone"`
 	Password string
+}
+
+func NewFromUrlValues(values url.Values) User {
+	return User{
+		Name: values.Get("name"),
+		Email: values.Get("email"),
+		Password: values.Get("password"),
+	}
 }
