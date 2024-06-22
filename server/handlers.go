180caa680f85fee96a93c0ac24351334d28895ee
CANITEY <mohamedmed01345@gmail.com>
2024-06-22 20:20:39 +0300

    created an auth/login backend function

--- server/handlers.go
+++ server/handlers.go
@@ -6,6 +6,10 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type toast struct {
+	Msg string
+	Status string
+}
 
 
 func (s *Server) authRoutes() {
@@ -24,19 +28,16 @@ func (s *Server) authRoutes() {
 				"passwords doesn't match",
 				"danger",
 			}
-			return c.Render(200, "toast", data)
+			return c.Render(500, "toast", data)
 		}
 
 		user := models.NewFromUrlValues(values)
 		if err := s.d.CreateUser(user); err != nil {
-			data := struct{
-				Msg string
-				Status string
-			}{
+			data := toast{
 				err.Error(),
 				"danger",
 			}
-			return c.Render(200, "toast", data)
+			return c.Render(500, "toast", data)
 		}
 		data := struct{
 			Msg string
@@ -46,5 +47,45 @@ func (s *Server) authRoutes() {
 			"success",
 		}
 		return c.Render(200, "toast", data)
+	})
+
+	s.S.POST("/login", func(c echo.Context) error {
+		email := c.FormValue("email")
+		password := c.FormValue("password")
+
+
+		if email == "" || password == "" {
+			data := toast{
+				"email or password are empty",
+				"danger",
+			}
+			return c.Render(403, "toast", data)
+		}
+		user, err := s.d.CheckUser(email, password)
+		if err != nil {
+			data := toast{
+				err.Error(),
+				"danger",
+			}
+			return c.Render(403, "toast", data)
+		}
+
+		if user == nil {
+			data := toast{
+				"user doesn't exist",
+				"danger",
+			}
+			return c.Render(403, "toast", data)
+		}
+
+		if user.Email != email || user.Password != password {
+			data := toast{
+				"email or password is invalid",
+				"danger",
+			}
+			return c.Render(403, "toast", data)
+		}
+
+		return c.Render(200, "profile", user)
 	})
 }
